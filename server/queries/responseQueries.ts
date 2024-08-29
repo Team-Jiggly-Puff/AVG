0025862c2a914edb563d66bec34c38e130cad4a2
Tdjergian <152138733+Tdjergian@users.noreply.github.com>
2024-08-28 18:04:09 -0700

    accidentally writing to dev

--- server/queries/responseQueries.ts
+++ server/queries/responseQueries.ts
@@ -5,3 +5,32 @@ INSERT INTO repsponses (user_id, option_id)
 VALUES ($1, $2)
 RETURNING _id;
 `;
+
+//Right now this is not being sorted by anything. might need to have it sorted by topic or something
+const getResponsesByUser = `
+SELECT users.username, polls.topic, questions.question, options.option, option_id
+FROM responses
+JOIN users ON users._id = responses.user_id
+JOIN options ON options._id = responses.option_id
+JOIN questions ON questions._id = options.question_id
+JOIN polls ON polls._id = questions.poll_id
+WHERE users.username = $1;
+`;
+
+const changeResponse = `
+UPDATE responses
+SET option_id = $1
+WHERE _id = $2
+`;
+
+const deleteResponseBy_id = `
+DELETE FROM responses
+WHERE _id = $1
+`;
+
+module.exports = {
+    postResponse,
+    getResponsesByUser,
+    changeResponse,
+    deleteResponseBy_id,
+}
--- server/queries/userQueries.ts
+++ server/queries/userQueries.ts
@@ -11,19 +11,9 @@ VALUES ($1, $2, $3, $4, $5, $6)
 RETURNING _id;
 `;
 
-//Right now this is not being sorted by anything. might need to have it sorted by topic or something
-const getUserResponses = `
-SELECT users.username, polls.topic, questions.question, options.option, option_id
-FROM responses
-JOIN users ON users._id = responses.user_id
-JOIN options ON options._id = responses.option_id
-JOIN questions ON questions._id = options.question_id
-JOIN polls ON polls._id = questions.poll_id
-WHERE users.username = $1;
-`;
+
 
 module.exports = {
     getUserByID,
     createUser,
-    getUserResponses,
 }
