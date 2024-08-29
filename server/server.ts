a275529b6d66814a9df14c1738b211ef03c07579
Alex Thorne <alexandra.dr.thorne@gmail.com>
2024-08-28 18:08:11 -0700

    server should work

--- server/server.ts
+++ server/server.ts
@@ -9,8 +9,17 @@ const port = process.env.PORT || 3000
 
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, '../client')));
+
 app.get('/', (req: Request, res: Response, next: NextFunction) => {
-    res.send("Express + TypeScript Server");
+    try {
+        res.send('index.html');
+    } catch (err) {
+        return next({
+            log: 'Error sending index.html to client',
+            message: { err: 'Server error loading page'}
+        });
+    }
 });
 
 app.listen(port, () => {
