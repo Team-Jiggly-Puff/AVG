168ae1ef498475c8e8722ada53a90577aaf357d0
Tdjergian <152138733+Tdjergian@users.noreply.github.com>
2024-09-03 18:04:53 -0700

    putting contrllers in routers and fixing queries for req.params instead of req.body

--- src/server/routes/pollRoutes.ts
+++ src/server/routes/pollRoutes.ts
@@ -1,19 +1,19 @@
 import express, { Request, Response, NextFunction } from 'express';
-const { createPoll, getSpecificPoll } = require('../controllers/pollController.ts');
-
+const { createPoll, getSpecificPoll, getAllTopics } = require('../controllers/pollController.ts');
+const { getUser } = require('../controllers/userController.ts');
 const router = express.Router();
 
-router.get('/all', (req: Request, res: Response, next: NextFunction) => {
-
+router.get('/topics', getAllTopics, (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).json(res.locals.topics);
 });
 
 router.get('/:id', getSpecificPoll, (req: Request, res: Response, next: NextFunction) => {
-    res.status(200).json(res.locals.polls);
+    res.status(200).json(res.locals.poll);
 });
 
-router.post('/new', createPoll, (req: Request, res: Response, next: NextFunction) => {
+router.post('/new', getUser, createPoll, (req: Request, res: Response, next: NextFunction) => {
 
-})
+});
 
 router.post('/vote', (req: Request, res: Response, next: NextFunction) => {
 
