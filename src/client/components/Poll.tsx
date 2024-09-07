f0fe73d7b4e659372553721b31bbe781441eb580
Tdjergian <152138733+Tdjergian@users.noreply.github.com>
2024-09-07 10:16:30 -0700

    merged with dev and saved Luis's changes on specific poll page

--- src/client/components/Poll.tsx
+++ src/client/components/Poll.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect , useState } from "react";
 import { useParams } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 interface pollInfo{
